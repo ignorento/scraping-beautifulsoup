6090bcd23849c8d785d696260be4382f2f9977f6
Oleg Babin <babin.architect@gmail.com>
2023-04-30 22:41:45 +0300

    update core

--- core/views.py
+++ core/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 
 from movies.models import Movie
-from services import ScrapeMoviesService
+from shows.models import TvShows
+from services import ScrapeMoviesService, ScrapeTvShowsService
+
 
 def index(request):
     service = ScrapeMoviesService()
@@ -23,5 +25,59 @@ def index(request):
         else:
             movie = Movie(**top_movie)
             movie.save()
+
+    service_shows = ScrapeTvShowsService()
+    top_shows = service_shows.get_top_movies()
+
+    for top_show in top_shows:
+        show = (
+            TvShows.objects.
+            filter(
+                poster_image=top_show.get('poster_image'),
+                title=top_show.get('title'),
+                year=top_show.get('year')
+            )
+            .first()
+        )
+        if show:
+            show.rating = top_show.get('rating')
+            show.save()
+        else:
+            show = TvShows(
+                poster_image=top_show.get('poster_image'),
+                title=top_show.get('title'),
+                year=top_show.get('year'),
+                rating=top_show.get('rating'),
+            )
+            show.save()
+
+    return render(request, "core/index.html")
+
+
+def shows(request):
+    service_shows = ScrapeTvShowsService()
+    top_shows = service_shows.get_top_shows()
+
+    for top_show in top_shows:
+        show = (
+            TvShows.objects.
+            filter(
+                title=top_show.get('title'),
+                year=top_show.get('year')
+            )
+            .first()
+        )
+        if show:
+            show.poster_image = top_show.get('poster_image')
+            show.rating = top_show.get('rating')
+            show.save()
+        else:
+            show = TvShows(
+                poster_image=top_show.get('poster_image'),
+                title=top_show.get('title'),
+                year=top_show.get('year'),
+                rating=top_show.get('rating'),
+            )
+            show.save()
 
     return render(request, "core/index.html")
